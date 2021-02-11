1ae3bf32dee85a202283dead7c11947a584d05eb
jay-tux <sys.jonas@yahoo.com>
2021-02-11 18:01:12 +0100

    Maybe, Either monads

---
+++ src/Either.kt
@@ -0,0 +1,37 @@
+class Either<E, A> private constructor(data: A?, err: E?) {
+    val data: A? = data
+    val error: E? = err
+
+    constructor(data: A) : this(data, null) { }
+    constructor(error: E, overload: Boolean) : this(null, error) { }
+
+    fun <B> fmap(f: (A) -> B): Either<E, B> {
+        return if(this.data == null) Either(error!!, false) else Either(f(data))
+    }
+
+    infix fun <B> `*?*`(f: (A) -> B): Either<E, B> {
+        return fmap(f)
+    }
+
+    fun <B> ap(f: Either<E, (A) -> B>): Either<E, B> {
+        return if(this.data == null || f.data == null) Either(error!!, false) else Either((f.data)(data))
+    }
+
+    fun <B> `***`(f: Either<E, (A) -> B>): Either<E, B> {
+        return ap(f)
+    }
+
+    fun <B> bind(f: (A) -> Either<E, B>): Either<E, B> {
+        return if(this.data == null) Either(error!!, false) else f(this.data)
+    }
+
+    infix fun <B> `**=`(f: (A) -> Either<E, B>): Either<E, B> {
+        return bind(f)
+    }
+
+    companion object {
+        fun <E, A> pure(data: A): Either<E, A> {
+            return Either(data)
+        }
+    }
+}
---
+++ src/Maybe.kt
@@ -0,0 +1,35 @@
+class Maybe<T>(data: T?) {
+    private val data: T? = data
+
+    constructor() : this(null) {}
+
+    fun <T2> fmap(f: (T) -> T2): Maybe<T2> {
+        return if(data == null) Maybe() else Maybe(f(data))
+    }
+
+    infix fun <T2> `*?*`(f: (T) -> T2): Maybe<T2> {
+        return fmap(f)
+    }
+
+    fun <T2> ap(f: Maybe<(T) -> T2>): Maybe<T2> {
+        return if(data == null || f.data == null) Maybe() else Maybe((f.data)(data))
+    }
+
+    infix fun <T2> `***`(f: Maybe<(T) -> T2>): Maybe<T2> {
+        return ap(f)
+    }
+
+    fun <T2> bind(f: (T) -> Maybe<T2>): Maybe<T2> {
+        return if(data == null) Maybe() else f(data)
+    }
+
+    infix fun <T2> `**=`(f: (T) -> Maybe<T2>): Maybe<T2> {
+        return bind(f)
+    }
+
+    companion object {
+        fun <T> pure(data:T):Maybe<T> {
+            return Maybe(data)
+        }
+    }
+}
