3bd57e0a19485ac3c9e7ad4230b7f4814ef8bba2
jay-tux <sys.jonas@yahoo.com>
2021-02-11 18:25:16 +0100

    ID monad (yes I know, it's a useless monad...)

---
+++ src/com/jay/monads/Id.kt
@@ -0,0 +1,15 @@
+package com.jay.monads
+
+class Id<T>(private val data: T) {
+
+    fun <T2> fmap(f: (T) -> T2): Id<T2> { return Id(f(data)) }
+    infix fun <T2> `*?*`(f: (T) -> T2): Id<T2> { return fmap(f) }
+    fun <T2> ap(f: Id<(T) -> T2>): Id<T2> { return Id(f.data(data)) }
+    infix fun <T2> `***`(f: Id<(T) -> T2>): Id<T2> { return ap(f) }
+    fun <T2> bind(f: (T) -> Id<T2>): Id<T2> { return f(data) }
+    infix fun <T2> `**=`(f: (T) -> Id<T2>): Id<T2> { return bind(f) }
+
+    companion object {
+        fun <T> pure(data: T): Id<T> { return Id(data) }
+    }
+}
